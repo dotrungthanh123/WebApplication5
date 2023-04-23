c8df84ad98814033efd9248b4c32f4160e06170e
ManhCoder2704 <manh9403@gmail.com>
2023-04-23 17:16:53 +0700

    Manh

---
+++ WebApplication5/Controllers/CartController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication5.Data;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly WebApplication5Context _context;
+
+        public CartController(WebApplication5Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Cart
+        public async Task<IActionResult> Index()
+        {
+            var webApplication5Context = _context.OrderDetails.Include(o => o.Event).Include(o => o.TicketOrder);
+            return View(await webApplication5Context.ToListAsync());
+        }
+
+        // GET: Cart/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails
+                .Include(o => o.Event)
+                .Include(o => o.TicketOrder)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetail);
+        }
+
+        // GET: Cart/Create
+        public IActionResult Create()
+        {
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId");
+            ViewData["OrderId"] = new SelectList(_context.TicketOrders, "OrderId", "OrderId");
+            return View();
+        }
+
+        // POST: Cart/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,OrderId,Quantity")] OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", orderDetail.EventId);
+            ViewData["OrderId"] = new SelectList(_context.TicketOrders, "OrderId", "OrderId", orderDetail.OrderId);
+            return View(orderDetail);
+        }
+
+        // GET: Cart/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", orderDetail.EventId);
+            ViewData["OrderId"] = new SelectList(_context.TicketOrders, "OrderId", "OrderId", orderDetail.OrderId);
+            return View(orderDetail);
+        }
+
+        // POST: Cart/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventId,OrderId,Quantity")] OrderDetail orderDetail)
+        {
+            if (id != orderDetail.EventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderDetailExists(orderDetail.EventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", orderDetail.EventId);
+            ViewData["OrderId"] = new SelectList(_context.TicketOrders, "OrderId", "OrderId", orderDetail.OrderId);
+            return View(orderDetail);
+        }
+
+        // GET: Cart/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails
+                .Include(o => o.Event)
+                .Include(o => o.TicketOrder)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetail);
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.OrderDetails == null)
+            {
+                return Problem("Entity set 'WebApplication5Context.OrderDetails'  is null.");
+            }
+            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            if (orderDetail != null)
+            {
+                _context.OrderDetails.Remove(orderDetail);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderDetailExists(int id)
+        {
+          return (_context.OrderDetails?.Any(e => e.EventId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebApplication5/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Allevents()
+        {
+            return View();
+        }
+    }
+}
