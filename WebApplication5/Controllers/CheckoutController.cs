2132f2a521af31433dd5f4f54ac70cbd95cab1a3
ManhCoder2704 <manh9403@gmail.com>
2023-04-25 08:44:19 +0700

    Manh up

---
+++ WebApplication5/Controllers/CheckoutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication5.Data;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly WebApplication5Context _context;
+
+        public CheckoutController(WebApplication5Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Checkout
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
